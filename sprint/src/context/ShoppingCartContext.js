877f49996cb554cf6ebd35da143d72cc3dd85e9a
RyanGuinchard <ryanguinchard9@hotmail.com>
2023-12-12 15:43:24 -0330

    Inital Setup of files

---
+++ sprint/src/context/ShoppingCartContext.js
@@ -0,0 +1,9 @@
+import React, { createContext, useState} from 'react'
+
+const ShoppingCartContext = () => {
+  return (
+    <div>ShoppingCartContext</div>
+  )
+}
+
+export default ShoppingCartContext
