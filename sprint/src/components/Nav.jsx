a0df60d2ee1acf3f63558777716a36c8d1025b94
malerie-earle <malerie.augot@gmail.com>
2023-12-14 16:00:18 -0330

    navigation update -  dec 14 - malerie

--- sprint/src/components/Nav.jsx
+++ sprint/src/components/Nav.jsx
@@ -1,10 +1,32 @@
 import { Outlet, Link } from "react-router-dom";
-
+          {/* I have the Checkout link ready to uncomment once it is active. 
+          Was throwing an error for me bc it was an empty link, 
+          Love you all <3 especially you ryan*/}
+          {/* <Link to="/checkout">
+            <div className="navBox">
+              <h3>Checkout</h3>
+            </div>
+          </Link> */}
+          // jsx hates comments lol - malerie
 const Nav = () => {
   return (
     <>
+    <nav>
       <div className="navBar">
-        <nav>
+
+
+        {/* <div class="wrap">
+                  <div class="search">
+                    <input type="text" class="searchTerm" placeholder="Search..." />
+                    <button type="submit" class="searchButton">
+                      <i class="fa fa-search">
+                          <img src = "Images/search_icon_125165.png" id = "searchIcon" /> 
+                      </i>
+                    </button>
+                  </div>
+              </div> */}
+
+
           <Link to="/">
             <div className="navBox" id="nav1">
               <h3>Home</h3>
@@ -17,16 +39,8 @@ const Nav = () => {
             </div>
           </Link>
           <br />
-          {/* I have the Checkout link ready to uncomment once it is active. 
-          Was throwing an error for me bc it was an empty link, 
-          Love you all <3 especially you ryan*/}
-          {/* <Link to="/checkout">
-            <div className="navBox">
-              <h3>Checkout</h3>
-            </div>
-          </Link> */}
-        </nav>
       </div>
+      </nav>
       <Outlet />
     </>
   );
