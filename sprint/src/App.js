265e0af750f8e424ed1c86486c93b56ff91cdc55
RyanGuinchard <ryanguinchard9@hotmail.com>
2023-12-14 00:00:06 -0330

    Dec13 - Setup context file

--- sprint/src/App.js
+++ sprint/src/App.js
@@ -1,27 +1,22 @@
 import "./styles/App.css";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import ProductList from "./components/ProductList";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav.jsx";
 import Home from "./pages/Home.jsx";
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/contact" element={<Contact />} /> */}
-      </Routes>
-    </BrowserRouter>
-      {/* <Router>
-        <ShoppingCartProvider>
-          <Switch>
-            <Route path="/" exact Component={ProductList} />
-          </Switch>
-        </ShoppingCartProvider>
-      </Router> */}
+      <ShoppingCartProvider>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </ShoppingCartProvider>
     </>
   );
 }
--- sprint/src/context/ShoppingCartContext.js
+++ sprint/src/context/ShoppingCartContext.js
@@ -1,9 +1,32 @@
-import React, { createContext, useState} from 'react'
+import React, { createContext, useState, useContext } from "react";
 
-const ShoppingCartContext = () => {
+const ShoppingCartContext = createContext();
+
+export const useShoppingCart = () => {
+  return useContext(ShoppingCartContext);
+};
+
+export const ShoppingCartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (itemId) => {
+    const updatedCart = cartItems.filter((item) => item.id !== itemId);
+    setCartItems(updatedCart);
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+
   return (
-    <div>ShoppingCartContext</div>
-  )
-}
-
-export default ShoppingCartContext;
+    <ShoppingCartContext.Provider value={contextValue}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
