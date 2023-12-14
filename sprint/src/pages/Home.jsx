4d91b9f05b273f5ac14fe158fd5875a7205b4733
malerie-earle <malerie.augot@gmail.com>
2023-12-14 14:41:45 -0330

    categories & feature added - dec 14 - malerie

--- sprint/src/pages/Home.jsx
+++ sprint/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import productsData from "../data/Products.JSON"; 
 import nlcircle from "../images/NLcircle.png";
-import newfieNook from "../images/newfieNook.png";
+import newfieNook from "../images/newfienook2.png";
 import "../styles/index.css";
 import Nav from "../components/Nav.jsx";
 
@@ -35,14 +35,38 @@ const Home = () => {
         <img src={nlcircle} alt="NL Circle" className="nlCircle" />
         <img src={newfieNook} alt="Newfie Nook Title" className="newfieNookTitle" />
       </div>
-      <div className="features">
-        <h1>- Features -</h1>
-        
-        <div>
-
+      <div className = "categories">
+        <div className = "category2" id = "title">
+          Categories:
+        </div>
+        <div className = "category" id = "cat1">
+          <h1 className = "catTitle">Bath & Body</h1>
+        </div>
+        <div className = "category" id = "cat2">
+        <h1 className = "catTitle">Clothing</h1>
+        </div>
+        <div className = "category" id = "cat3">
+        <h1 className = "catTitle">Food & Drink</h1>
+        </div>
+        <div className = "category" id = "cat4">
+        <h1 className = "catTitle">Merchandise</h1>
+        </div>
+        <div className = "category" id = "cat5">
+        <h1 className = "catTitle">Seasonal</h1>
         </div>
       </div>
-    </div>
+
+      <div className = "section">
+      <h1 className = "featureTitle">- Features -</h1><br />
+      <div className="features">
+        <div className = "feature" id = "feat1"></div>
+        <div className = "feature" id = "feat2"></div>
+        <div className = "feature" id = "feat3"></div>
+        <div className = "feature" id = "feat4"></div>
+        </div>
+        </div>
+        </div>
+
   );
 };
 
